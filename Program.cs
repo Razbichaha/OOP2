6916bb2133eb7079e03b5543c7636469e7cff30d
Сергей <seregabur@live.ru>
2022-04-22 21:28:19 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,23 +1,164 @@
 ﻿using System;
+using System.Threading;
 
 namespace OOP2
 {
-//    Создать класс игрока, у которого есть поля с его положением в x,y.
-//Создать класс отрисовщик, с методом, который отрисует игрока.
+    //    Создать класс игрока, у которого есть поля с его положением в x,y.
+    //Создать класс отрисовщик, с методом, который отрисует игрока.
 
-//Попрактиковаться в работе со свойствами.
+    //Попрактиковаться в работе со свойствами.
+
 
     class Program
     {
         static void Main(string[] args)
         {
+            string[,] map = { {"###################################" },
+                              {"#                                 #" },
+                              {"#    #####              #####     #" },
+                              {"#     ###                ###      #" },
+                              {"#                #                #" },
+                              {"#                #                #" },
+                              {"#                #                #" },
+                              {"#                #                #" },
+                              {"#                #                #" },
+                              {"#       ####################      #" },
+                              {"#                                 #" },
+                              {"###################################" }};
 
+            char[,] сharMap = CreateCharMap(map);
 
-            Console.WriteLine("Hello World!");
+            VisualizeMap(сharMap);
+
+            int positionX = 1;
+            int positionY = 1;
+            char player = '@';
+
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(positionX, positionY);
+            Console.Write(player);
+            ControlOfGame(ref positionX, ref positionY, player, сharMap);
+
         }
+
+        static void ControlOfGame(ref int positionX, ref int positionY, char player, char[,] map)
+        {
+            int movementX;
+            int movementY;
+            int sleep = 300;
+            bool isContinuedGame = true;
+
+            while (isContinuedGame)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+
+                            movementY = -1; movementX = 0;
+                            MovePlaer(ref positionX, ref positionY, movementX, movementY, player, map);
+
+                            break;
+                        case ConsoleKey.DownArrow:
+
+                            movementY = 1; movementX = 0;
+                            MovePlaer(ref positionX, ref positionY, movementX, movementY, player, map);
+
+                            break;
+                        case ConsoleKey.LeftArrow:
+
+                            movementX = -1; movementY = 0;
+                            MovePlaer(ref positionX, ref positionY, movementX, movementY, player, map);
+
+                            break;
+                        case ConsoleKey.RightArrow:
+
+                            movementX = 1; movementY = 0;
+                            MovePlaer(ref positionX, ref positionY, movementX, movementY, player, map);
+
+                            break;
+                        case ConsoleKey.Escape:
+
+                            isContinuedGame = false;
+
+                            break;
+                    }
+                }
+                Thread.Sleep(sleep);
+            }
+        }
+
+        static void MovePlaer(ref int positionX, ref int positionY, int movementX, int movementY, char player, char[,] map)
+        {
+
+            char stopMovement = '#';
+
+            if (map[positionY + movementY, positionX + movementX] != stopMovement)
+            {
+                Console.SetCursorPosition(positionX, positionY);
+                Console.Write(" ");
+                positionX += movementX;
+                positionY += movementY;
+                Console.SetCursorPosition(positionX, positionY);
+                Console.Write(player);
+            }
+        }
+
+        static void VisualizeMap(char[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+            Console.WindowHeight = map.GetLength(0);
+            Console.WindowWidth = map.GetLength(1);
+
+            int bufferLimit = 1;
+            Console.BufferHeight = Console.WindowHeight + bufferLimit;
+            Console.BufferWidth = Console.WindowWidth + bufferLimit;
+
+        }
+
+        static char[,] CreateCharMap(string[,] map)
+        {
+            char[,] charMap = new char[map.GetLength(0), map[0, 0].Length];
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                string tempString = map[i, 0];
+
+                for (int j = 0; j < tempString.Length; j++)
+                {
+                    charMap[i, j] = tempString[j];
+                }
+            }
+
+            return charMap;
+        }
+
     }
 
+
     class Plaer
+    {
+        private int positionX = 1;
+        private int positionY = 1;
+
+
+
+
+
+
+    }
+
+    class ShowPlaer
     {
 
 
