dc0ffc636af1a47d937e81fba23d0e7297a16ec5
Сергей <seregabur@live.ru>
2022-04-22 15:07:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP2
+{
+//    Создать класс игрока, у которого есть поля с его положением в x,y.
+//Создать класс отрисовщик, с методом, который отрисует игрока.
+
+//Попрактиковаться в работе со свойствами.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Plaer
+    {
+
+
+    }
+}
